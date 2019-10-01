e3af7b844b7763342afc46dc9682a4f093e83c8b
Luca Facchin <32875729+lucafa@users.noreply.github.com>
2019-10-02 00:54:46 +0200

    Add files via upload

---
+++ datatype_sizes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+printf("|int|%d|\n", sizeof(int));
+printf("|unsigned int|%d|\n", sizeof(unsigned int));
+printf("|short int|%d|\n", sizeof(short int));
+printf("|long int|%d|\n", sizeof(long int));
+printf("|long long int|%d|\n", sizeof(long long int));
+printf("|float|%d|\n", sizeof(float));
+printf("|char|%d|\n", sizeof(char));
+
+}
